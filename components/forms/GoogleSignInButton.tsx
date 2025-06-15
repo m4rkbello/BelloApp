e22a759d59737d19daacf04ed2f9fa7498180119
m4rkbello <markbello10041999@gmail.com>
2025-06-15 18:37:48 +0800

    ADDED

---
+++ components/forms/GoogleSignInButton.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function GoogleSignInButton() {
+    return (
+        <View>
+            <Text>GoogleSignInButton</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
